fd41feb399c8a0d877625db19f059e94f7c5beca
João Victor Teófilo Salgado <jvictorteo2000@hotmail.com>
2025-03-05 00:19:44 -0300

    GeoPackage creation.

--- star_chart/commands/observe.py
+++ star_chart/commands/observe.py
@@ -7,6 +7,7 @@ import geopandas as gpd
 import typer
 from geopandas import GeoDataFrame
 from pandas import DataFrame
+from shapely import LineString, MultiLineString, Polygon
 from skyfield.api import Star, load, wgs84
 from skyfield.projections import build_stereographic_projection
 from typing_extensions import Annotated
@@ -15,6 +16,9 @@ logger = logging.getLogger(__name__)
 
 # Set global epoch year (1991.25)
 EPOCH_YEAR = 1991.25
+
+# Default EPSG code
+EPSG_CODE = 3857
 
 
 def _build_stars_geo_data_frame(
@@ -30,9 +34,9 @@ def _build_stars_geo_data_frame(
 
     logger.info("Building stars geodataframe")
     stars_gdf = gpd.GeoDataFrame(
-        stars_df,
+        stars_df[["hip", "magnitude"]].set_index("hip"),
         geometry=gpd.points_from_xy(stars_df["x"], stars_df["y"]),
-        crs=4326,
+        crs=EPSG_CODE,
     )
     return stars_gdf
 
@@ -69,7 +73,23 @@ def _build_constellation_edges_geo_data_frame(
     constellation_edges_df["x"], constellation_edges_df["y"] = projection(
         constellation_edges_positions
     )
-    return
+
+    constellation_edges_grouped_series = (
+        constellation_edges_df.groupby(["iau_abbreviation", "iau_name", "segment"])
+        .apply(lambda group: LineString(zip(group["x"], group["y"])))
+        .groupby(["iau_abbreviation", "iau_name"])
+        .apply(lambda group: MultiLineString(list(group)))
+    )
+
+    constellation_edges_grouped_df = constellation_edges_grouped_series.to_frame(
+        name="geometry"
+    )
+    constellation_edges_gdf = GeoDataFrame(
+        constellation_edges_grouped_df,
+        geometry="geometry",
+        crs=EPSG_CODE,
+    )
+    return constellation_edges_gdf
 
 
 def _build_constellation_boundaries_geo_data_frame(
@@ -100,7 +120,21 @@ def _build_constellation_boundaries_geo_data_frame(
     constellation_boundaries_df["x"], constellation_boundaries_df["y"] = projection(
         constellation_boundaries_positions
     )
-    return
+
+    constellation_boundaries_grouped_series = constellation_boundaries_df.groupby(
+        ["iau_abbreviation", "iau_name"]
+    ).apply(lambda group: Polygon(list(zip(group["x"], group["y"]))))
+
+    constellation_boundaries_grouped_df = (
+        constellation_boundaries_grouped_series.to_frame(name="geometry")
+    )
+    constellation_boundaries_gdf = GeoDataFrame(
+        constellation_boundaries_grouped_df,
+        geometry="geometry",
+        crs=EPSG_CODE,
+    )
+
+    return constellation_boundaries_gdf
 
 
 def observe(
@@ -195,12 +229,22 @@ def observe(
             "SELECT * FROM constellation_boundaries"
         ).df()
 
+    logger.info("Building 'stars' GeoDataFrame")
     stars_gdf = _build_stars_geo_data_frame(
         earth,
         timescale,
         projection,
         stars_df,
     )
+    logger.info("Saving 'stars' GeoDataFrame to %s/stars", output_geopackage_file_path)
+    stars_gdf.to_file(
+        output_geopackage_file_path,
+        driver="GPKG",
+        layer="stars",
+        encoding="utf-8",
+    )
+
+    logger.info("Building 'constellation_edges' GeoDataFrame")
     constellation_edges_gdf = _build_constellation_edges_geo_data_frame(
         earth,
         timescale,
@@ -208,7 +252,18 @@ def observe(
         stars_df,
         constellation_edges_df,
     )
+    logger.info(
+        "Saving 'constellation_edges' GeoDataFrame to %s/constellation_edges",
+        output_geopackage_file_path,
+    )
+    constellation_edges_gdf.to_file(
+        output_geopackage_file_path,
+        driver="GPKG",
+        layer="constellation_edges",
+        encoding="utf-8",
+    )
 
+    logger.info("Building 'constellation_boundaries' GeoDataFrame")
     constellation_boundaries_gdf = _build_constellation_boundaries_geo_data_frame(
         earth,
         timescale,
@@ -216,180 +271,13 @@ def observe(
         constellation_edges_df,
         constellation_boundaries_df,
     )
-
-    # logger.info("Downloading HYG data")
-    # # Load HYG data (Hipparcos-Yale-Gliese) to get proper names, constellations and bayer
-    # with load.open(star_name_url) as compressed_downloaded_file:
-    #     extracted_downloaded_file = extract_file(compressed_downloaded_file)
-    #     star_names_df = pd.read_csv(
-    #         extracted_downloaded_file,
-    #         index_col="hip",
-    #         usecols=["hip", "proper", "bayer", "con"],
-    #     )
-    # star_names_df.columns = ("proper", "bayer", "abbreviation")
-    # star_names_df["abbreviation"] = star_names_df["abbreviation"].str.upper()
-    # star_names_df["bayer"] = star_names_df["bayer"].str.split("-").str[0]
-    # star_names_df["greek"] = star_names_df["bayer"].map(ascii_to_greek_letters)
-
-    # # Join columns
-    # stars_df = stars_df.merge(
-    #     star_names_df, how="inner", left_index=True, right_index=True
-    # )
-    # stars_df.index = stars_df.index.astype("int")
-
-    # # Build geodataframe
-    # logger.info("Building stars geodataframe")
-    # stars_gdf = gpd.GeoDataFrame(
-    #     stars_df,
-    #     geometry=gpd.points_from_xy(stars_df["x"], stars_df["y"]),
-    #     crs=4326,
-    # )  # type: ignore
-    # stars_gdf = stars_gdf[
-    #     ["magnitude", "proper", "bayer", "greek", "abbreviation", "geometry"]
-    # ]
-
-    # # Set CRS
-    # stars_gdf = stars_gdf.set_crs(epsg=4326)  # type: ignore
-
-    # # Export to .gpkg
-    # logger.info(f"Saving stars geodataframe to {output_file_path}")
-    # stars_gdf.to_file(output_file_path, driver="GPKG", layer="stars", encoding="utf-8")
-
-    # # Load constellation name data
-    # logger.info("Downloading constellation names")
-    # with load.open(constellation_name_url) as downloaded_file:
-    #     constellations_names_df = pd.read_csv(
-    #         downloaded_file,
-    #         delimiter="\t",
-    #         header=None,
-    #         names=["abbreviation", "name"],
-    #         index_col=0,
-    #         usecols=[0, 1],
-    #     )
-    # constellations_names_df.index = constellations_names_df.index.str.upper()
-
-    # # Load constellation edge data
-    # logger.info("Downloading constellation edges")
-    # with load.open(constellation_data_url) as downloaded_file:
-    #     constellations = stellarium.parse_constellations(downloaded_file)
-    # constellations_coordinate_pairs = sum(
-    #     (
-    #         [
-    #             [constellation[0].upper(), vertice[0], vertice[1]]
-    #             for vertice in constellation[1]
-    #         ]
-    #         for constellation in constellations
-    #     ),
-    #     [],
-    # )
-    # constellations_df = pd.DataFrame(
-    #     constellations_coordinate_pairs,
-    #     columns=["abbreviation", "start_hip", "end_hip"],
-    # )
-    # constellations_df.index.name = "id"
-
-    # # Merge with star coordinates
-    # logger.info("Obtaining constellation edge coordinates from star's coordinates")
-    # constellations_df = constellations_df.merge(
-    #     stars_df[["x", "y", "ra_degrees", "dec_degrees"]].add_prefix("start_"),
-    #     how="inner",
-    #     left_on="start_hip",
-    #     right_index=True,
-    # )
-    # constellations_df = constellations_df.merge(
-    #     stars_df[["x", "y", "ra_degrees", "dec_degrees"]].add_prefix("end_"),
-    #     how="inner",
-    #     left_on="end_hip",
-    #     right_index=True,
-    # )
-
-    # # Build geodataframe
-    # logger.info("Building constellation edges geodataframe")
-    # constellations_gdf = gpd.GeoDataFrame(
-    #     constellations_df,
-    #     geometry=constellations_df.apply(
-    #         lambda item: LineString(
-    #             [
-    #                 Point([item["start_x"], item["start_y"]]),
-    #                 Point([item["end_x"], item["end_y"]]),
-    #             ]
-    #         ),  # type: ignore
-    #         result_type=None,
-    #         axis=1,
-    #     ),  # type: ignore
-    #     crs=4326,
-    # )  # type: ignore
-    # constellations_gdf = constellations_gdf.dissolve(by="abbreviation").merge(
-    #     constellations_names_df, left_on="abbreviation", right_index=True
-    # )[["geometry", "name"]]
-
-    # # Set CRS
-    # constellations_gdf = constellations_gdf.set_crs(epsg=4326)  # type: ignore
-
-    # # Export to .gpkg
-    # logger.info(f"Saving constellation eges geodataframe to {output_file_path}")
-    # constellations_gdf.to_file(
-    #     output_file_path, driver="GPKG", layer="constellations", encoding="utf-8"
-    # )
-
-    # # Load constellation boundary data
-    # logger.info("Downloading constellation boundaries")
-    # with load.open(constellation_boundary_url) as downloaded_file:
-    #     colspecs = [(0, 9), (9, 18), (19, 23), (24, 25)]
-    #     names = ["ra_hours", "dec_degrees", "abbreviation", "type_point"]
-    #     boundaries_df = pd.read_fwf(
-    #         downloaded_file,
-    #         colspecs=colspecs,
-    #         names=names,
-    #     )
-
-    # # Filter boundary data by point type
-    # boundaries_df = boundaries_df[boundaries_df["type_point"] == "O"]
-
-    # # Create required columns
-    # boundaries_df.index.name = "id"
-    # boundaries_df["epoch_year"] = EPOCH_YEAR
-
-    # # Convert hours to degrees
-    # boundaries_df["ra_degrees"] = boundaries_df["ra_hours"]
-
-    # # Project coordinates
-    # logger.info("Projecting constellation boundaries coordinates")
-    # boundary_positions = earth.at(timescale).observe(Star.from_dataframe(boundaries_df))  # type: ignore
-    # boundaries_df["x"], boundaries_df["y"] = projection(boundary_positions)
-
-    # # Drop not used columns
-    # boundaries_df = boundaries_df.drop(columns=["type_point", "ra_hours", "epoch_year"])
-
-    # # Build geodataframe
-    # logger.info("Building constellation boundaries geodataframe")
-    # boundaries_gdf = gpd.GeoDataFrame(
-    #     boundaries_df,
-    #     geometry=gpd.points_from_xy(boundaries_df["x"], boundaries_df["y"]),
-    #     crs=4326,
-    # )  # type: ignore
-    # boundaries_gdf = (
-    #     boundaries_gdf.groupby("abbreviation")["geometry"]
-    #     .apply(lambda x: Polygon(x.tolist()))
-    #     .reset_index()
-    # )
-    # boundaries_gdf.index.name = "id"
-
-    # # Remove numbers from new abbreviations
-    # boundaries_gdf["new_abbreviation"] = boundaries_gdf["abbreviation"].str[:3]
-
-    # # Add constellation names
-    # boundaries_gdf = boundaries_gdf.merge(
-    #     constellations_names_df, left_on="new_abbreviation", right_index=True
-    # )[["geometry", "name", "new_abbreviation", "abbreviation"]]
-    # boundaries_gdf = boundaries_gdf.drop(columns=["new_abbreviation"])
-    # boundaries_gdf = boundaries_gdf.set_index("abbreviation", verify_integrity=True)
-
-    # # Set CRS
-    # boundaries_gdf = boundaries_gdf.set_crs(epsg=4326)  # type: ignore
-
-    # # Export to .gpkg
-    # logger.info(f"Saving constellation boundaries geodataframe to {output_file_path}")
-    # boundaries_gdf.to_file(
-    #     output_file_path, driver="GPKG", layer="boundaries", encoding="utf-8"
-    # )
+    logger.info(
+        "Saving 'constellation_boundaries' GeoDataFrame to %s/constellation_boundaries",
+        output_geopackage_file_path,
+    )
+    constellation_boundaries_gdf.to_file(
+        output_geopackage_file_path,
+        driver="GPKG",
+        layer="constellation_boundaries",
+        encoding="utf-8",
+    )
